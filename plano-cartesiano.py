8ae47e8eed976ae27cca75fd29c2255daecd3266
Lucas Zambam <lucaszambam7896@gmail.com>
2021-09-09 00:54:47 -0300

    first commit

---
+++ plano-cartesiano.py
@@ -0,0 +1,76 @@
+# Importando as biblioteca para plotar gráfico e a segunda para os calculos.
+
+import matplotlib.pyplot as plt
+import numpy as np 
+
+# Input das coordenadas.
+
+x1 = int(input("Insira x1 : "))
+x2 = int(input("Insira x2 : "))
+y1 = int(input("Insira y1 : "))
+y2 = int(input("Insira y2 : "))
+
+result = ((((x2 - x1 )**2) + ((y2-y1)**2) )**0.5)
+
+print("A distancia entre",(x1,x2),"e",(y1,y2),"é : ",result)
+
+
+xs = [0, x1, x2]
+ys = [0, y1, y2]
+colors = ['m', 'g', 'r']
+
+# Select length of axes and the space between tick labels
+xmin, xmax, ymin, ymax = -5, 5, -5, 5
+ticks_frequency = 1
+
+# Plot points
+fig, ax = plt.subplots(figsize=(10, 10))
+ax.scatter(xs, ys, c=colors)
+
+# Draw lines connecting points to axes
+for x, y, c in zip(xs, ys, colors):
+    ax.plot([x, x], [0, y], c=c, ls='--', lw=1.5, alpha=0.5)
+    ax.plot([0, x], [y, y], c=c, ls='--', lw=1.5, alpha=0.5)
+
+# Set identical scales for both axes
+ax.set(xlim=(xmin-1, xmax+1), ylim=(ymin-1, ymax+1), aspect='equal')
+
+# Set bottom and left spines as x and y axes of coordinate system
+ax.spines['bottom'].set_position('zero')
+ax.spines['left'].set_position('zero')
+
+# Remove top and right spines
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+
+# Create 'x' and 'y' labels placed at the end of the axes
+ax.set_xlabel('x', size=14, labelpad=-24, x=1.03)
+ax.set_ylabel('y', size=14, labelpad=-21, y=1.02, rotation=0)
+
+# Create custom major ticks to determine position of tick labels
+x_ticks = np.arange(xmin, xmax+1, ticks_frequency)
+y_ticks = np.arange(ymin, ymax+1, ticks_frequency)
+ax.set_xticks(x_ticks[x_ticks != 0])
+ax.set_yticks(y_ticks[y_ticks != 0])
+
+# Create minor ticks placed at each integer to enable drawing of minor grid
+# lines: note that this has no effect in this example with ticks_frequency=1
+ax.set_xticks(np.arange(xmin, xmax+1), minor=True)
+ax.set_yticks(np.arange(ymin, ymax+1), minor=True)
+
+# Draw major and minor grid lines
+ax.grid(which='both', color='grey', linewidth=1, linestyle='-', alpha=0.2)
+
+# Draw arrows
+arrow_fmt = dict(markersize=4, color='black', clip_on=False)
+ax.plot((1), (0), marker='>', transform=ax.get_yaxis_transform(), **arrow_fmt)
+ax.plot((0), (1), marker='^', transform=ax.get_xaxis_transform(), **arrow_fmt)
+
+# Draw a line between two points
+point1 = [x1, y1]
+point2 = [x2, y2]
+x_values = [point1[0], point2[0]]
+y_values = [point1[1], point2[1]]
+plt.plot(x_values, y_values)
+
+plt.show()
